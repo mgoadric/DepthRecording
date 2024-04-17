30220708c9f3b8d7898e1b8ecb16ec6aebd3eea3
Mark Goadrich <goadrich@hendrix.edu>
2024-04-17 11:32:39 -0500

    Loaded data into arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the world!");
+        File input = new File("input.txt");
+        ArrayList<Integer> depth = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(input);
+            while (sc.hasNextLine()) {
+                depth.add(Integer.parseInt(sc.nextLine()));
+            }
+            sc.close();
+        } catch (FileNotFoundException fnfe) {
+
+        }
+
+        System.out.println(depth.size() + " lines loaded.");
+
 
     }
 }
